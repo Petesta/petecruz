c06f458842ef5245feebec27d0279620c605b334
Petesta <Petesta@live.com>
2014-03-08 14:19:09 -0800

    Fix div containers and styling of posts.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:id])
+    @user = User.new(params[:user])
 
     if @user.save
       flash[:success] = "Successful"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create(email: "admin@admin.com", full_name: "admin", password: "admin", password_confirmation: "admin")
+
+Post.create(body:"Chef stuff", title: "Chef", user_id: 1)
+Post.create(body:"Rails stuff", title: "Rails", user_id: 1)
+Post.create(body:"Scala stuff", title: "Scala", user_id: 1)
